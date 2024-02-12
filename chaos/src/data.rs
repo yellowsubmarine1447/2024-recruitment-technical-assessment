47c2fc02622b4c776c71f2e07ba26ac0ad17d42c
yellowsubmarine1447 <cyril.subramanian@gmail.com>
2024-02-12 15:54:02 +1100

    feat: chaos q1

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -4,19 +4,33 @@ use serde::{Deserialize, Serialize};
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
 
-    let response = DataResponse {
-        
+    let mut response = DataResponse {
+        string_len: 0,
+        int_sum: 0,
     };
+    for entry in request.data {
+        match entry {
+            DataEntry::Int(num) => response.int_sum += num,
+            DataEntry::String(string) => response.string_len += string.len() as i32,
+        };
+    }
 
     (StatusCode::OK, Json(response))
 }
 
 #[derive(Deserialize)]
 pub struct DataRequest {
-    // Add any fields here
+    data: Vec<DataEntry>,
 }
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    string_len: i32,
+    int_sum: i32,
+}
+
+#[derive(Deserialize)]
+enum DataEntry {
+    Int(i32),
+    String(String),
 }
